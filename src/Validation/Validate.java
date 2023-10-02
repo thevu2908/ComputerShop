0b390479d236ec7635a7365211e2ca6f37c6a5a8
thevu29 <vnguyen132az@gmail.com>
2023-10-02 19:49:18 +0700

    validation

--- src/Validation/Validate.java
+++ src/Validation/Validate.java
@@ -1,4 +1,44 @@
 package Validation;
 
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
 public class Validate {
+    private static Pattern emailPattern = Pattern.compile("[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+");
+    public static boolean isValidEmail(String email) {
+        return email.matches(emailPattern.pattern());
+    }
+
+    public static boolean isValidPhone(String phone) {
+        return phone.matches("[0-9]{10}");
+    }
+
+    public static boolean isValidDate(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        sdf.setLenient(false);
+        try {
+            Date d = sdf.parse(date);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidNumber(String number, String text) {
+        try {
+            int num = Integer.parseInt(number);
+
+            if (num < 0) {
+                JOptionPane.showMessageDialog(null, text + " phải là số dương", "Lỗi", JOptionPane.ERROR_MESSAGE);
+                return false;
+            }
+
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, text + " phải là chữ số", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
 }
