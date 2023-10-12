732ce904bc34b8f7babe33d2937b25a1760a2ca6
NamNguyenSGU <102597360+NamNguyenSGU@users.noreply.github.com>
2023-10-12 19:33:11 +0700

    nam

--- src/connection/MyConnection.java
+++ src/connection/MyConnection.java
@@ -8,6 +8,7 @@ public class MyConnection {
     private static String url = "jdbc:mysql://localhost:3306/computershop";
     private static String user = "root";
     private static String password = "";
+    private Connection connection = null;
 
     public MyConnection() {
     }
@@ -16,4 +17,20 @@ public class MyConnection {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, user, password);
     }
+    public void closeConnection()  {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            else {
+                System.out.println("conn is not null!! \n");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
