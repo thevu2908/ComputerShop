687910741032740c40c514bffa93006acdf9b31d
thevu29 <vnguyen132az@gmail.com>
2023-10-27 13:38:05 +0700

    fix connection

--- src/connection/MyConnection.java
+++ src/connection/MyConnection.java
@@ -8,29 +8,9 @@ public class MyConnection {
     private static String url = "jdbc:mysql://localhost:3306/computershop";
     private static String user = "root";
     private static String password = "";
-    private Connection connection = null;
-
-    public MyConnection() {
-    }
 
     public static Connection getConnect() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, user, password);
     }
-    public void closeConnection()  {
-        try {
-            if (connection != null) {
-                connection.close();
-            }
-            else {
-                System.out.println("conn is not null!! \n");
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
-
-    }
-
 }
